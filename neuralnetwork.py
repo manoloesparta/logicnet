7904d05acb871525c6332cf88b12396e72b28121
Manuel Esparza <manoloesparta@totopoyo.local>
2018-08-20 13:25:11 -0700

    [update] weights normalization

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -9,14 +9,14 @@ class NeuralNetwork:
 
 		self.learning_rate = learning_rate
 
-		self.weights_input_hidden = np.random.randn(self.hidden_nodes, self.input_nodes)
-		self.weights_hidden_output = np.random.randn(self.output_nodes, self.hidden_nodes)
+		self.weights_input_hidden = np.random.randn(self.hidden_nodes, self.input_nodes) - 0.5
+		self.weights_hidden_output = np.random.randn(self.output_nodes, self.hidden_nodes) - 0.5
 
 	def train(self, train_data, target_data, epochs):
-		for i in range(epochs):
+		inputs = np.array(train_data, ndmin=2).T
+		targets = np.array(target_data).T
 
-			inputs = np.array(train_data, ndmin=2).T
-			targets = np.array(target_data).T
+		for i in range(epochs):
 
 			hidden_inputs = np.matmul(self.weights_input_hidden, inputs)
 			hidden_outputs = NeuralNetwork.sigmoid(hidden_inputs)
@@ -24,16 +24,16 @@ class NeuralNetwork:
 			final_inputs = np.matmul(self.weights_hidden_output, hidden_outputs)
 			final_outputs = NeuralNetwork.sigmoid(final_inputs)
 
-			output_error = (targets - final_outputs) ** 2
-			hidden_error = np.matmul(self.weights_hidden_output.T, output_error)
-
-			self.weights_hidden_output += self.learning_rate * np.matmul(NeuralNetwork.dsigmoid(final_inputs), hidden_outputs.T)
-			self.weights_input_hidden += self.learning_rate * np.matmul(NeuralNetwork.dsigmoid(hidden_inputs), inputs.T)
+			output_errors = (targets - final_outputs) ** 2
+			hidden_errors = np.matmul(self.weights_hidden_output.T, output_errors)
+			
+			self.weights_hidden_output += self.learning_rate * np.matmul(output_errors * (final_outputs * (1.0 - final_outputs)), hidden_outputs.T)	
+			self.weights_input_hidden += self.learning_rate * np.matmul(hidden_errors * (hidden_outputs * (1.0 - hidden_outputs)), inputs.T)
 
 		return 'trained'
 	
-	def predict(self, predict_data):
-		inputs = np.array(predict_data, ndmin=2).T
+	def predict(self, input_data):
+		inputs = np.array(input_data, ndmin=2).T
 
 		hidden_inputs = np.matmul(self.weights_input_hidden, inputs)
 		hidden_outputs = NeuralNetwork.sigmoid(hidden_inputs)
@@ -47,13 +47,7 @@ class NeuralNetwork:
 	def sigmoid(x):
 		return 1 / (1 + np.exp(-x))
 
-	@staticmethod
-	def dsigmoid(x):
-		return NeuralNetwork.sigmoid(x) * (1 - NeuralNetwork.sigmoid(x))
-
 if __name__ == '__main__':
-	
-	ann = NeuralNetwork(2,10,1,0.2)
-	ann.train([[0,1]],[0],100)
-
-	print(ann.predict([0,1]))
+	ann = NeuralNetwork(2,30,1,0.2)
+	ann.train([[0,0],[1,1]],[0,1],1000)
+	print(ann.predict([1,1]))
