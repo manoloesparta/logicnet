bd2ccf3971c4aac797aa166cb47beb45f69213c8
Manuel Esparza <manoloesparta@gmail.com>
2019-03-06 09:34:44 -0800

    [update matrix.py] matrix sum added, matrix transpose added to matrix class

--- ann/matrix.py
+++ ann/matrix.py
@@ -1,21 +1,44 @@
 from random import random
 
-
 class Matrix:
 
-    def __init__(self, rows, columns, zeros=False):
-        self.shape = (rows, columns)
+    def __init__(self, cols, rows, zeros=False):
+        self.cols = cols
         self.rows = rows
-        self.columns = columns
+        self.shape = (cols, rows)
 
         if zeros:
-            self.matrix = [[0 for _ in range(self.rows)] for _ in range(self.columns)]
+            self.matrix = [[0 for _ in range(self.rows)] for _ in range(self.cols)]
         else:
-            self.matrix = [[random() for _ in range(rows)] for _ in range(columns)]
+            self.matrix = [[random() for _ in range(self.rows)] for _ in range(self.cols)]
 
     def __repr__(self):
         raw = str(self.matrix).split('],')
         return ']\n'.join(raw)
+
+    def __add__(self, matrix):
+        assert self.shape == matrix.shape, 'Not compatible shapes'
+        cols, rows = self.shape
+        result = Matrix(rows, cols, zeros=True)
+
+        for i in range(cols):
+           for j in range(rows):
+              result.matrix[i][j] = self.matrix[i][j] + matrix.matrix[i][j]
+
+        return result
+
+    def T(self):
+        rows, cols = self.shape
+        result = Matrix(cols, rows, zeros=True)
+
+        for i in range(cols):
+            for j in range(rows):
+                result.matrix[i][j] = self.matrix[j][i]
+
+        self.shape = result.shape
+        self.cols, self.rows = result.cols, result.rows
+        self.matrix = result.matrix
+
 
     @staticmethod
     def matmul(matrix1, matrix2):
--- test_matrix.py
+++ test_matrix.py
@@ -1,9 +1,6 @@
 from ann.matrix import Matrix
 
-m1 = Matrix(3,3)
-m2 = Matrix(3,1)
-
-res = Matrix.matmul(m1, m2)
-
-print(res)
-
+m1 = Matrix(3,2)
+#m2 = Matrix(2,3)
+m1.T()
+print(m1)
